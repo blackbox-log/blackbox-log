51e4a3acb6ba4b4ec951c0e4590fd80fa77e285b
William Etheredge <me@wetheredge.com>
2022-08-20 06:19:48 -0500

    Properly organize benches

--- benches/read.rs
+++ benches/read.rs
@@ -4,9 +4,9 @@ use criterion::{criterion_group, criterion_main};
 use criterion::{BatchSize, Bencher, BenchmarkId, Criterion, Throughput};
 use std::iter;
 
-macro_rules! bench {
+macro_rules! get_bench {
     ($func:expr) => {
-        bench!(bench, $func)
+        get_bench!(bench, $func)
     };
     ($name:ident, $func:expr) => {
         fn $name(b: &mut Bencher, input: &[u8]) {
@@ -15,99 +15,74 @@ macro_rules! bench {
     };
 }
 
-fn uvar(c: &mut Criterion) {
-    bench!(encoding::read_uvar);
+fn variable(c: &mut Criterion) {
+    get_bench!(ubench, encoding::read_uvar);
+    get_bench!(ibench, encoding::read_ivar);
 
-    let mut group = c.benchmark_group("unsigned variable byte");
+    let mut group = c.benchmark_group("variable byte");
 
-    let input: &[u8] = &[0x00];
-    group.throughput(Throughput::Bytes(input.len() as u64));
-    group.bench_with_input("minimum", input, bench);
+    let benches: [(_, &[u8]); 2] = [("min", &[0]), ("max", &[0xFF, 0xFF, 0xFF, 0xFF, 0xF8])];
+    for (name, input) in benches {
+        group.throughput(Throughput::Bytes(input.len() as u64));
 
-    let input: &[u8] = &[0xFF, 0xFF, 0xFF, 0xFF, 0xF8];
-    group.throughput(Throughput::Bytes(input.len() as u64));
-    group.bench_with_input("maximum", input, bench);
+        let id = BenchmarkId::new("unsigned", name);
+        group.bench_with_input(id, input, ubench);
+
+        let id = BenchmarkId::new("signed", name);
+        group.bench_with_input(id, input, ibench);
+    }
 
     for extra_bytes in 1..5 {
         let mut input: Vec<_> = iter::repeat(0x80).take(extra_bytes).collect();
         input.push(0);
         let input = input.as_slice();
+        let name = &format!("{}-byte 0", input.len());
 
         group.throughput(Throughput::Bytes(input.len() as u64));
 
-        let id = BenchmarkId::new("multi-byte 0", extra_bytes);
-        group.bench_with_input(id, input, bench);
+        let id = BenchmarkId::new("unsigned", name);
+        group.bench_with_input(id, input, ubench);
+
+        let id = BenchmarkId::new("signed", name);
+        group.bench_with_input(id, input, ibench);
     }
 
     group.finish();
 }
 
-fn ivar(c: &mut Criterion) {
-    bench!(encoding::read_ivar);
+fn elias_delta(c: &mut Criterion) {
+    get_bench!(ubench, encoding::read_u32_elias_delta);
+    get_bench!(ibench, encoding::read_i32_elias_delta);
 
-    let mut group = c.benchmark_group("signed variable byte");
-
-    let input: &[u8] = &[0x00];
-    group.throughput(Throughput::Bytes(input.len() as u64));
-    group.bench_with_input("minimum", input, bench);
-
-    let input: &[u8] = &[0xFF, 0xFF, 0xFF, 0xFF, 0xF8];
-    group.throughput(Throughput::Bytes(input.len() as u64));
-    group.bench_with_input("positive maximum", input, bench);
-
-    for extra_bytes in 1..5 {
-        let mut input: Vec<_> = iter::repeat(0x80).take(extra_bytes).collect();
-        input.push(0);
-        let input = input.as_slice();
-
-        group.throughput(Throughput::Bytes(input.len() as u64));
-
-        let id = BenchmarkId::new("multi-byte 0", extra_bytes);
-        group.bench_with_input(id, input, bench);
-    }
-
-    group.finish();
-}
-
-fn u32_elias_delta(c: &mut Criterion) {
-    bench!(encoding::read_u32_elias_delta);
-
-    let mut group = c.benchmark_group("unsigned Elias delta");
+    let mut group = c.benchmark_group("Elias delta");
 
     let min: &[u8] = &[0x80];
     let max: &[u8] = &[0x04, 0x1F, 0xFF, 0xFF, 0xFF, 0xC0];
     for (input, name) in [(min, "minimum"), (max, "maximum")] {
         group.throughput(Throughput::Bytes(input.len() as u64));
-        group.bench_with_input(name, input, bench);
-    }
 
-    group.finish();
-}
+        let id = BenchmarkId::new("unsigned", name);
+        group.bench_with_input(id, input, ubench);
 
-fn i32_elias_delta(c: &mut Criterion) {
-    bench!(encoding::read_u32_elias_delta);
-
-    let mut group = c.benchmark_group("signed Elias delta");
-
-    let min: &[u8] = &[0x80];
-    let max: &[u8] = &[0x04, 0x1F, 0xFF, 0xFF, 0xFF, 0x80];
-    for (input, name) in [(min, "minimum"), (max, "positive maximum")] {
-        group.throughput(Throughput::Bytes(input.len() as u64));
-        group.bench_with_input(name, input, bench);
+        let id = BenchmarkId::new("signed", name);
+        group.bench_with_input(id, input, ibench);
     }
 
     group.finish();
 }
 
 fn tagged_16(c: &mut Criterion) {
-    bench!(bench_v1, |data| encoding::read_tagged_16(
-        LogVersion::V1,
-        data
-    ));
-    bench!(bench_v2, |data| encoding::read_tagged_16(
-        LogVersion::V2,
-        data
-    ));
+    use LogVersion::{V1, V2};
+
+    fn get_bench(version: LogVersion) -> impl FnMut(&mut Bencher, &[u8]) {
+        move |b, input| {
+            b.iter_batched_ref(
+                || Biterator::new(input),
+                |input| encoding::read_tagged_16(version, input),
+                BatchSize::SmallInput,
+            )
+        }
+    }
 
     fn input(first: u8, zeros: usize) -> Vec<u8> {
         iter::once(first)
@@ -117,33 +92,22 @@ fn tagged_16(c: &mut Criterion) {
 
     let mut group = c.benchmark_group("tagged 16");
 
-    let benches: [(&str, Vec<u8>); 4] = [
-        ("zeros", input(0x00, 0)),
-        ("nibbles", input(0x55, 2)),
-        ("bytes", input(0xAA, 4)),
-        ("16 bits", input(0xFF, 8)),
+    let benches = [
+        (BenchmarkId::from_parameter("zeros"), input(0x00, 0), V1),
+        (BenchmarkId::new("v1", "nibbles"), input(0x55, 2), V1),
+        (BenchmarkId::new("v2", "nibbles"), input(0x55, 2), V2),
+        (BenchmarkId::from_parameter("bytes"), input(0xAA, 4), V1),
+        (BenchmarkId::from_parameter("16 bits"), input(0xFF, 8), V1),
     ];
-    for (name, input) in benches.iter() {
+    for (id, input, version) in benches {
         let input = input.as_slice();
 
         group.throughput(Throughput::Bytes(input.len() as u64));
-        let id = BenchmarkId::new("v1", name);
-        group.bench_with_input(id, input, bench_v1);
-
-        let id = BenchmarkId::new("v2", name);
-        group.throughput(Throughput::Bytes(input.len() as u64));
-        group.bench_with_input(id, input, bench_v2);
+        group.bench_with_input(id, input, get_bench(version));
     }
 
     group.finish();
 }
 
-criterion_group!(
-    benches,
-    uvar,
-    ivar,
-    u32_elias_delta,
-    i32_elias_delta,
-    tagged_16
-);
+criterion_group!(benches, variable, elias_delta, tagged_16);
 criterion_main!(benches);
