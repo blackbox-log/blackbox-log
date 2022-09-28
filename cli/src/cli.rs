51181b20fe27e30913acc6b955fcd2db18b47b29
William Etheredge <me@wetheredge.com>
2022-09-28 18:53:37 -0500

    blackbox_decode: clap v4

--- cli/src/cli.rs
+++ cli/src/cli.rs
@@ -1,5 +1,5 @@
 use blackbox::parser::Config;
-use clap::{AppSettings, ArgAction, Parser, ValueEnum, ValueHint};
+use clap::{ArgAction, Parser, ValueEnum, ValueHint};
 use std::path::PathBuf;
 use tracing_subscriber::filter::LevelFilter;
 
@@ -7,29 +7,29 @@ use tracing_subscriber::filter::LevelFilter;
 pub enum AmperageUnit {
     #[default]
     Raw,
-    #[clap(name = "mA", alias = "ma")]
+    #[value(name = "mA", alias = "ma")]
     Milliamps,
-    #[clap(name = "A", alias = "a")]
+    #[value(name = "A", alias = "a")]
     Amps,
 }
 
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, ValueEnum)]
 pub enum FrameTime {
     #[default]
-    #[clap(name = "us", alias = "micros")]
+    #[value(name = "us", alias = "micros")]
     Microseconds,
-    #[clap(name = "s")]
+    #[value(name = "s")]
     Seconds,
 }
 
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, ValueEnum)]
 pub enum HeightUnit {
     #[default]
-    #[clap(name = "cm")]
+    #[value(name = "cm")]
     Centimeters,
-    #[clap(name = "m")]
+    #[value(name = "m")]
     Meters,
-    #[clap(name = "ft")]
+    #[value(name = "ft")]
     Feet,
 }
 
@@ -38,11 +38,11 @@ pub enum RotationUnit {
     #[default]
     Raw,
 
-    #[clap(name = "deg/s", alias = "deg")]
+    #[value(name = "deg/s", alias = "deg")]
     /// Degrees/second
     Degrees,
 
-    #[clap(name = "rad/s", alias = "rad")]
+    #[value(name = "rad/s", alias = "rad")]
     /// Radians/second
     Radians,
 }
@@ -52,10 +52,10 @@ pub enum AccelerationUnit {
     #[default]
     Raw,
 
-    #[clap(name = "g")]
+    #[value(name = "g")]
     Gs,
 
-    #[clap(name = "m/s2")]
+    #[value(name = "m/s2")]
     /// Meters per second squared
     Mps2,
 }
@@ -78,121 +78,120 @@ pub enum VBatUnit {
     #[default]
     Raw,
 
-    #[clap(name = "mV", alias = "mv")]
+    #[value(name = "mV", alias = "mv")]
     Millivolts,
 
-    #[clap(name = "V", alias = "v")]
+    #[value(name = "V", alias = "v")]
     Volts,
 }
 
 #[derive(Debug, Parser)]
-#[clap(about, author, version)]
-#[clap(global_setting(AppSettings::DeriveDisplayOrder))]
+#[command(about, author, version)]
 pub(crate) struct Cli {
     // TODO: accept - for stdin
-    #[clap(required(true), value_name = "log", value_hint = ValueHint::FilePath)]
+    #[arg(required(true), value_name = "log", value_hint = ValueHint::FilePath)]
     /// One or more logs to parse
     pub logs: Vec<PathBuf>,
 
-    // #[clap(short, long, action = ArgAction::Help)]
+    // #[arg(short, long, action = ArgAction::Help)]
     // /// Print this help message
     // help: bool,
-    #[clap(short, long, value_name = "index")]
+    #[arg(short, long, value_name = "index")]
     /// Choose which log from the file should be decoded or omit to decode all
     pub index: Vec<usize>,
 
-    #[clap(long)]
+    #[arg(long)]
     /// Print the limits and range of each field
     pub limits: bool,
 
-    #[clap(long)]
+    #[arg(long)]
     /// Write log to stdout instead of a file
     pub stdout: bool,
 
-    #[clap(long, value_enum, default_value_t, value_name = "unit")]
+    #[arg(long, value_enum, default_value_t, value_name = "unit")]
     pub unit_amperage: AmperageUnit,
 
     // TODO: --unit-frame-time
-    #[clap(long, value_enum, default_value_t, value_name = "unit")]
+    #[arg(long, value_enum, default_value_t, value_name = "unit")]
     pub unit_height: HeightUnit,
 
-    #[clap(long, value_enum, default_value_t, value_name = "unit")]
+    #[arg(long, value_enum, default_value_t, value_name = "unit")]
     pub unit_rotation: RotationUnit,
 
-    #[clap(long, value_enum, default_value_t, value_name = "unit")]
+    #[arg(long, value_enum, default_value_t, value_name = "unit")]
     pub unit_acceleration: AccelerationUnit,
 
-    #[clap(long, value_enum, default_value_t, value_name = "unit")]
+    #[arg(long, value_enum, default_value_t, value_name = "unit")]
     pub unit_gps_speed: GpsSpeedUnit,
 
-    #[clap(long, value_enum, default_value_t, value_name = "unit")]
+    #[arg(long, value_enum, default_value_t, value_name = "unit")]
     pub unit_vbat: VBatUnit,
 
-    #[clap(long, default_value_t, value_name = "offset", alias = "alt-offset")]
+    #[arg(long, default_value_t, value_name = "offset", alias = "alt-offset")]
     /// Altitude offset in meters
     pub altitude_offset: u16,
 
-    #[clap(long)]
+    #[arg(long)]
     /// Merge GPS data into the main CSV file instead of writing it separately
     pub merge_gps: bool,
 
-    #[clap(long, alias = "simulate-current-meter")]
+    #[arg(long, alias = "simulate-current-meter")]
     /// Simulate a virtual current meter using throttle data
     pub sim_current_meter: bool,
 
-    #[clap(
+    #[arg(
         long,
-        requires = "sim-current-meter",
+        requires = "sim_current_meter",
         alias = "sim-current-meter-scale",
         alias = "simulate-current-meter-scale"
     )]
     /// Override the flight controller's current scale when simulating the current meter
     pub current_scale: bool,
 
-    #[clap(
+    #[arg(
         long,
-        requires = "sim-current-meter",
+        requires = "sim_current_meter",
         alias = "sim-current-meter-offset",
         alias = "simulate-current-meter-offset"
     )]
     /// Override the flight controller's current offset when simulating the current meter
     pub current_offset: bool,
 
-    #[clap(long, alias = "simulate-imu")]
+    #[arg(long, alias = "simulate-imu")]
     /// Compute tilt, roll, and heading information from gyro, accelerometer, and magnetometer data
     pub sim_imu: bool,
 
-    #[clap(
+    #[arg(
         long,
-        requires = "sim-imu",
+        requires = "sim_imu",
         alias = "include-imu-deg",
         alias = "include-imu-degrees"
     )]
     /// Include (deg) in the tilt/roll/heading header
     pub imu_deg: bool,
 
-    #[clap(long, requires = "sim-imu", alias = "imu-ignore-mag")]
+    #[arg(long, requires = "sim_imu", alias = "imu-ignore-mag")]
     /// Ignore magnetometer when computing heading
     pub ignore_mag: bool,
 
     // TODO
-    // #[clap(long)]
+    // #[arg(long)]
     // /// Set magnetic declination in degrees.minutes format (e.g. -12.58 for New York)
     // declination: (),
     //
     // TODO
-    // #[clap(long)]
+    // #[arg(long)]
     // /// Set magnetic declination in decimal degrees (e.g. -12.97 for New York)
     // declination_dec: (),
-    #[clap(long)]
+    #[arg(long)]
     /// Show raw field values without applying predictions
     pub raw: bool,
 
-    #[clap(short, long, action = ArgAction::Count, conflicts_with = "verbose")]
+    #[arg(short, long, action = ArgAction::Count, conflicts_with = "verbose")]
     /// Reduce output
     pub quiet: u8,
 
-    #[clap(short, long, action = ArgAction::Count, alias = "debug")]
+    #[arg(short, long, action = ArgAction::Count, alias = "debug")]
     /// Increase output
     pub verbose: u8,
 }
