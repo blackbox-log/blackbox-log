60836d612c2bb781c100e1eb8041b17b75bd02e7
William Etheredge <me@wetheredge.com>
2022-10-05 16:21:12 -0500

    Update BF flight modes & add inav

--- src/betaflight.rs
+++ src/betaflight.rs
@@ -1,5 +1,3 @@
-use alloc::vec::Vec;
-
 // flightLogDisarmReason_e in core.h
 generate_disarm_reason! {
     ArmingDisabled = 0,
@@ -13,108 +11,17 @@ generate_disarm_reason! {
     SerialCommand = 8,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub enum FlightMode {
-    Angle,
-    Horizon,
-    Mag,
-    Baro,
-    GpsHome,
-    GpsHold,
-    HeadFree,
-    Passthru,
-    RangeFinder,
-    Failsafe,
-}
-
-impl FlightMode {
-    const fn to_bit(self) -> usize {
-        match self {
-            Self::Angle => 0,
-            Self::Horizon => 1,
-            Self::Mag => 2,
-            Self::Baro => 3,
-            Self::GpsHome => 4,
-            Self::GpsHold => 5,
-            Self::HeadFree => 6,
-            Self::Passthru => 8,
-            Self::RangeFinder => 9,
-            Self::Failsafe => 10,
-        }
-    }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct FlightModeFlags(u32);
-
-impl FlightModeFlags {
-    pub const fn new(flags: u32) -> Self {
-        Self(flags)
-    }
-
-    const fn is_bit_set(self, bit: usize) -> bool {
-        (self.0 & (1 << bit)) > 0
-    }
-
-    pub const fn is_mode_set(self, mode: FlightMode) -> bool {
-        self.is_bit_set(mode.to_bit())
-    }
-
-    pub const fn angle(self) -> bool {
-        self.is_mode_set(FlightMode::Angle)
-    }
-
-    pub const fn horizon(self) -> bool {
-        self.is_mode_set(FlightMode::Horizon)
-    }
-
-    pub const fn mag(self) -> bool {
-        self.is_mode_set(FlightMode::Mag)
-    }
-
-    pub const fn baro(self) -> bool {
-        self.is_mode_set(FlightMode::Baro)
-    }
-
-    pub const fn gps_home(self) -> bool {
-        self.is_mode_set(FlightMode::GpsHome)
-    }
-
-    pub const fn gps_hold(self) -> bool {
-        self.is_mode_set(FlightMode::GpsHold)
-    }
-
-    pub const fn headfree(self) -> bool {
-        self.is_mode_set(FlightMode::HeadFree)
-    }
-
-    pub const fn passthru(self) -> bool {
-        self.is_mode_set(FlightMode::Passthru)
-    }
-
-    pub const fn rangefinder(self) -> bool {
-        self.is_mode_set(FlightMode::RangeFinder)
-    }
-
-    pub const fn failsafe(self) -> bool {
-        self.is_mode_set(FlightMode::Failsafe)
-    }
-
-    pub fn to_modes(self) -> Vec<FlightMode> {
-        [
-            FlightMode::Angle,
-            FlightMode::Horizon,
-            FlightMode::Mag,
-            FlightMode::Baro,
-            FlightMode::GpsHome,
-            FlightMode::GpsHold,
-            FlightMode::HeadFree,
-            FlightMode::Passthru,
-            FlightMode::RangeFinder,
-            FlightMode::Failsafe,
-        ]
-        .into_iter()
-        .filter(|&mode| self.is_mode_set(mode))
-        .collect()
-    }
+// flightModeFlags_e in runtime_config.h
+generate_flight_mode! {
+    Angle / angle = 0,
+    Horizon / horizon = 1,
+    Mag / mag = 2,
+    Baro / baro = 3,
+    GpsHome / gps_home = 4,
+    GpsHold / gps_hold = 5,
+    HeadFree / head_free = 6,
+    Passthru / passthru = 8,
+    RangeFinder / range_finder = 9,
+    Failsafe / failsafe = 10,
+    GpsRescue / gps_rescue = 11,
 }
--- src/common.rs
+++ src/common.rs
@@ -1,5 +1,6 @@
 use alloc::borrow::ToOwned;
 use core::fmt;
+use core::marker::PhantomData;
 use core::str::FromStr;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
@@ -73,5 +74,57 @@ macro_rules! generate_disarm_reason {
                 }
             }
         }
+    }
+}
+
+macro_rules! generate_flight_mode {
+    ( $( $mode:ident / $mode_fn:ident = $bit:expr ),+ $(,)? ) => {
+        #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+        #[repr(u32)]
+        pub enum FlightMode {
+            $( $mode = 1 << $bit ),+
+        }
+
+        impl FlightMode {
+            const fn to_bit(self) -> u32 {
+                match self {
+                    $( Self::$mode => $bit ),+
+                }
+            }
+        }
+
+        impl crate::common::FlightModeFlags<FlightMode> {
+            #[inline]
+            pub const fn is_mode_set(self, mode: FlightMode) -> bool {
+                self.is_bit_set(mode.to_bit())
+            }
+
+            $(
+                pub const fn $mode_fn(self) -> bool {
+                    self.is_mode_set(FlightMode::$mode)
+                }
+            )+
+
+            pub fn to_modes(self) -> alloc::vec::Vec<FlightMode> {
+                [ $( FlightMode::$mode ),+ ]
+                    .into_iter()
+                    .filter(|&mode| self.is_mode_set(mode))
+                    .collect()
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct FlightModeFlags<T>(u32, PhantomData<T>);
+
+impl<T> FlightModeFlags<T> {
+    pub const fn new(flags: u32) -> Self {
+        Self(flags, PhantomData)
+    }
+
+    #[inline]
+    pub(crate) const fn is_bit_set(&self, bit: u32) -> bool {
+        (self.0 & (1 << bit)) > 0
     }
 }
