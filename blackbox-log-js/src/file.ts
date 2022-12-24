002aa1661cbaf75e2b8c0a26bd480221e11ecf05
William Etheredge <me@wetheredge.com>
2022-12-23 23:08:30 -0600

    Set up prettier

--- blackbox-log-js/src/file.ts
+++ blackbox-log-js/src/file.ts
@@ -2,65 +2,65 @@ import { WasmPointer, WasmObject, Module } from './wasm';
 import { Log, Headers } from './log';
 
 export class File implements WasmObject {
-  readonly #wasm: Module;
-  readonly #ptr: WasmPointer;
+	readonly #wasm: Module;
+	readonly #ptr: WasmPointer;
 
-  #headers: Array<WeakRef<Headers>> = [];
-  #logs: Array<WeakRef<Log>> = [];
+	#headers: Array<WeakRef<Headers>> = [];
+	#logs: Array<WeakRef<Log>> = [];
 
-  constructor(wasm: WebAssembly.Instance & { exports: Module }, data: Uint8Array) {
-    this.#wasm = wasm.exports;
+	constructor(wasm: WebAssembly.Instance & { exports: Module }, data: Uint8Array) {
+		this.#wasm = wasm.exports;
 
-    const dataPtr = this.#wasm.data_alloc(data.length);
-    if (dataPtr == 0) {
-      throw new Error('file is too large');
-    }
+		const dataPtr = this.#wasm.data_alloc(data.length);
+		if (dataPtr == 0) {
+			throw new Error('file is too large');
+		}
 
-    const buffer = new Uint8Array(this.#wasm.memory.buffer, dataPtr, data.length);
-    buffer.set(data);
+		const buffer = new Uint8Array(this.#wasm.memory.buffer, dataPtr, data.length);
+		buffer.set(data);
 
-    const filePtr = this.#wasm.file_new(dataPtr, data.length);
-    this.#ptr = new WasmPointer(filePtr, this.#wasm.file_free);
-  }
+		const filePtr = this.#wasm.file_new(dataPtr, data.length);
+		this.#ptr = new WasmPointer(filePtr, this.#wasm.file_free);
+	}
 
-  free() {
-    this.#ptr.free();
-  }
+	free() {
+		this.#ptr.free();
+	}
 
-  get isAlive(): boolean {
-    return this.#ptr.isAlive;
-  }
+	get isAlive(): boolean {
+		return this.#ptr.isAlive;
+	}
 
-  get logCount(): number {
-    const ptr = this.#ptr.ptr;
-    return this.#wasm.file_logCount(ptr);
-  }
+	get logCount(): number {
+		const ptr = this.#ptr.ptr;
+		return this.#wasm.file_logCount(ptr);
+	}
 
-  parseHeaders(index: number): Headers | undefined {
-    if (index >= this.logCount) {
-      return undefined;
-    }
+	parseHeaders(index: number): Headers | undefined {
+		if (index >= this.logCount) {
+			return undefined;
+		}
 
-    if (index in this.#headers && this.#headers[index].deref()?.isAlive) {
-      return this.#headers[index].deref();
-    }
+		if (index in this.#headers && this.#headers[index].deref()?.isAlive) {
+			return this.#headers[index].deref();
+		}
 
-    const headers = new Headers(this.#wasm, this.#ptr.ptr, index);
-    this.#headers[index] = new WeakRef(headers);
-    return headers;
-  }
+		const headers = new Headers(this.#wasm, this.#ptr.ptr, index);
+		this.#headers[index] = new WeakRef(headers);
+		return headers;
+	}
 
-  parseLog(index: number): Log | undefined {
-    if (index >= this.logCount) {
-      return undefined;
-    }
+	parseLog(index: number): Log | undefined {
+		if (index >= this.logCount) {
+			return undefined;
+		}
 
-    if (this.#logs[index]?.deref()?.isAlive) {
-      return this.#logs[index].deref();
-    }
+		if (this.#logs[index]?.deref()?.isAlive) {
+			return this.#logs[index].deref();
+		}
 
-    const log = new Log(this.#wasm, this.#ptr.ptr, index);
-    this.#logs[index] = new WeakRef(log);
-    return log;
-  }
+		const log = new Log(this.#wasm, this.#ptr.ptr, index);
+		this.#logs[index] = new WeakRef(log);
+		return log;
+	}
 }
--- blackbox-log-js/src/log.ts
+++ blackbox-log-js/src/log.ts
@@ -3,91 +3,91 @@ import { WasmPointer, WasmObject, Module } from './wasm';
 import { getOptionalWasmStr, getWasmStr } from './str';
 
 interface HeaderView {
-  firmwareRevision: string;
-  boardInfo?: string;
-  craftName?: string;
+	firmwareRevision: string;
+	boardInfo?: string;
+	craftName?: string;
 }
 
 export class Headers implements HeaderView, WasmObject {
-  readonly #wasm: Module;
-  readonly #ptr: WasmPointer;
+	readonly #wasm: Module;
+	readonly #ptr: WasmPointer;
 
-  constructor(wasm: Module, file: number, log: number) {
-    this.#wasm = wasm;
-    const ptr = this.#wasm.file_getHeaders(file, log);
-    this.#ptr = new WasmPointer(ptr, this.#wasm.headers_free);
-  }
+	constructor(wasm: Module, file: number, log: number) {
+		this.#wasm = wasm;
+		const ptr = this.#wasm.file_getHeaders(file, log);
+		this.#ptr = new WasmPointer(ptr, this.#wasm.headers_free);
+	}
 
-  free() {
-    this.#ptr.free();
-  }
+	free() {
+		this.#ptr.free();
+	}
 
-  get isAlive(): boolean {
-    return this.#ptr.isAlive;
-  }
+	get isAlive(): boolean {
+		return this.#ptr.isAlive;
+	}
 
-  @Memoize()
-  get firmwareRevision(): string {
-    const revision = this.#wasm.headers_firmwareRevision(this.#ptr.ptr, false);
-    return getWasmStr(revision, this.#wasm);
-  }
+	@Memoize()
+	get firmwareRevision(): string {
+		const revision = this.#wasm.headers_firmwareRevision(this.#ptr.ptr, false);
+		return getWasmStr(revision, this.#wasm);
+	}
 
-  @Memoize()
-  get boardInfo(): string | undefined {
-    const name = this.#wasm.headers_boardInfo(this.#ptr.ptr, false);
-    return getOptionalWasmStr(name, this.#wasm);
-  }
+	@Memoize()
+	get boardInfo(): string | undefined {
+		const name = this.#wasm.headers_boardInfo(this.#ptr.ptr, false);
+		return getOptionalWasmStr(name, this.#wasm);
+	}
 
-  @Memoize()
-  get craftName(): string | undefined {
-    const name = this.#wasm.headers_craftName(this.#ptr.ptr, false);
-    return getOptionalWasmStr(name, this.#wasm);
-  }
+	@Memoize()
+	get craftName(): string | undefined {
+		const name = this.#wasm.headers_craftName(this.#ptr.ptr, false);
+		return getOptionalWasmStr(name, this.#wasm);
+	}
 }
 
 export class Log implements HeaderView, WasmObject {
-  readonly #wasm: Module;
-  readonly #ptr: WasmPointer;
+	readonly #wasm: Module;
+	readonly #ptr: WasmPointer;
 
-  constructor(wasm: Module, file: number, log: number) {
-    this.#wasm = wasm;
-    const ptr = this.#wasm.file_getLog(file, log);
-    this.#ptr = new WasmPointer(ptr, this.#wasm.log_free);
-  }
+	constructor(wasm: Module, file: number, log: number) {
+		this.#wasm = wasm;
+		const ptr = this.#wasm.file_getLog(file, log);
+		this.#ptr = new WasmPointer(ptr, this.#wasm.log_free);
+	}
 
-  free() {
-    this.#ptr.free();
-  }
+	free() {
+		this.#ptr.free();
+	}
 
-  get isAlive(): boolean {
-    return this.#ptr.isAlive;
-  }
+	get isAlive(): boolean {
+		return this.#ptr.isAlive;
+	}
 
-  @Memoize()
-  get firmwareRevision(): string {
-    const revision = this.#wasm.headers_firmwareRevision(this.#ptr.ptr, true);
-    return getWasmStr(revision, this.#wasm);
-  }
+	@Memoize()
+	get firmwareRevision(): string {
+		const revision = this.#wasm.headers_firmwareRevision(this.#ptr.ptr, true);
+		return getWasmStr(revision, this.#wasm);
+	}
 
-  @Memoize()
-  get boardInfo(): string | undefined {
-    const name = this.#wasm.headers_boardInfo(this.#ptr.ptr, true);
-    return getOptionalWasmStr(name, this.#wasm);
-  }
+	@Memoize()
+	get boardInfo(): string | undefined {
+		const name = this.#wasm.headers_boardInfo(this.#ptr.ptr, true);
+		return getOptionalWasmStr(name, this.#wasm);
+	}
 
-  @Memoize()
-  get craftName(): string | undefined {
-    const name = this.#wasm.headers_craftName(this.#ptr.ptr, true);
-    return getOptionalWasmStr(name, this.#wasm);
-  }
+	@Memoize()
+	get craftName(): string | undefined {
+		const name = this.#wasm.headers_craftName(this.#ptr.ptr, true);
+		return getOptionalWasmStr(name, this.#wasm);
+	}
 
-  @Memoize()
-  get mainFrameCount(): number {
-    return this.#wasm.log_mainFrameCount(this.#ptr.ptr);
-  }
+	@Memoize()
+	get mainFrameCount(): number {
+		return this.#wasm.log_mainFrameCount(this.#ptr.ptr);
+	}
 
-  @Memoize()
-  get gpsFrameCount(): number {
-    return this.#wasm.log_gpsFrameCount(this.#ptr.ptr);
-  }
+	@Memoize()
+	get gpsFrameCount(): number {
+		return this.#wasm.log_gpsFrameCount(this.#ptr.ptr);
+	}
 }
