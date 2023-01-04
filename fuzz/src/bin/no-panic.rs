1b60b33c8c4f6699e50fdacc20025599df596a71
William Etheredge <me@wetheredge.com>
2023-01-04 02:12:21 -0600

    Fix fuzz tests for `DataParser` rewrite

--- fuzz/src/bin/no-panic.rs
+++ fuzz/src/bin/no-panic.rs
@@ -1,6 +1,13 @@
 #![no_main]
 
+use blackbox_log::prelude::*;
+
 blackbox_fuzz::fuzz_target!(|data: &[u8]| {
     let f = blackbox_log::File::new(data);
-    for _ in f.parse_iter() {}
+    for mut reader in f.iter() {
+        let Ok(headers) = Headers::parse(&mut reader) else { return; };
+        let mut data = DataParser::new(reader, &headers);
+
+        while data.next().is_some() {}
+    }
 });
