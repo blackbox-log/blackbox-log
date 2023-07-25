70b5d413e18baf5411f4363852ad99c4ef768e50
William Etheredge <me@wetheredge.com>
2023-07-24 21:05:30 -0500

    Add test for msrv in devcontainer

--- tests/msrv.rs
+++ tests/msrv.rs
@@ -3,6 +3,8 @@ use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 use std::path::PathBuf;
 
+const MSRV: &str = env!("CARGO_PKG_RUST_VERSION");
+
 #[test]
 fn readme() -> io::Result<()> {
     let readme = File::open("README.md")?;
@@ -17,7 +19,26 @@ fn readme() -> io::Result<()> {
     let (msrv, _) = msrv.split_once(')').unwrap();
     let (_, msrv) = msrv.rsplit_once('=').unwrap();
 
-    assert_eq!(env!("CARGO_PKG_RUST_VERSION"), msrv);
+    assert_eq!(MSRV, msrv);
+
+    Ok(())
+}
+
+#[test]
+fn devcontainer() -> io::Result<()> {
+    let devcontainer = File::open(".devcontainer/default/devcontainer.json")?;
+    let devcontainer = BufReader::new(devcontainer);
+
+    let msrv = devcontainer
+        .lines()
+        .map(Result::unwrap)
+        .find(|line| line.contains(r#""msrv":"#))
+        .expect("MSRV build arg");
+
+    let (_, msrv) = msrv.split_once(':').unwrap();
+    let msrv = msrv.split('"').nth(1).unwrap();
+
+    assert_eq!(MSRV, msrv);
 
     Ok(())
 }
@@ -43,7 +64,7 @@ fn toolchain_file() -> io::Result<()> {
     let (_, msrv) = msrv.split_once('=').unwrap();
     let msrv = msrv.trim().trim_matches('"');
 
-    assert_eq!(env!("CARGO_PKG_RUST_VERSION"), msrv);
+    assert_eq!(MSRV, msrv);
 
     Ok(())
 }
