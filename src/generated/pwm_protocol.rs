4b8f6b3d167e651f0bf6b7667d75b15044396efd
W Etheredge <me@wetheredge.com>
2023-11-11 18:24:38 -0600

    Support Betaflight 4.5.0

--- src/generated/pwm_protocol.rs
+++ src/generated/pwm_protocol.rs
@@ -58,25 +58,45 @@ impl PwmProtocol {
     pub(crate) fn new(raw: u32, fw: crate::headers::InternalFirmware) -> Option<Self> {
         use crate::headers::InternalFirmware::*;
         match (raw, fw) {
-            (0u32, Betaflight4_2 | Betaflight4_3 | Betaflight4_4 | Inav5 | Inav6 | Inav7) => {
-                Some(Self::Standard)
+            (
+                0u32,
+                Betaflight4_2 | Betaflight4_3 | Betaflight4_4 | Betaflight4_5 | Inav5 | Inav6
+                | Inav7,
+            ) => Some(Self::Standard),
+            (
+                1u32,
+                Betaflight4_2 | Betaflight4_3 | Betaflight4_4 | Betaflight4_5 | Inav5 | Inav6
+                | Inav7,
+            ) => Some(Self::Oneshot125),
+            (2u32, Betaflight4_2 | Betaflight4_3 | Betaflight4_4 | Betaflight4_5) => {
+                Some(Self::Oneshot42)
             }
-            (1u32, Betaflight4_2 | Betaflight4_3 | Betaflight4_4 | Inav5 | Inav6 | Inav7) => {
-                Some(Self::Oneshot125)
-            }
-            (2u32, Betaflight4_2 | Betaflight4_3 | Betaflight4_4) => Some(Self::Oneshot42),
             (2u32, Inav5 | Inav6 | Inav7) => Some(Self::Multishot),
-            (3u32, Betaflight4_2 | Betaflight4_3 | Betaflight4_4) => Some(Self::Multishot),
+            (3u32, Betaflight4_2 | Betaflight4_3 | Betaflight4_4 | Betaflight4_5) => {
+                Some(Self::Multishot)
+            }
             (3u32, Inav5 | Inav6 | Inav7) => Some(Self::Brushed),
-            (4u32, Betaflight4_2 | Betaflight4_3 | Betaflight4_4) => Some(Self::Brushed),
+            (4u32, Betaflight4_2 | Betaflight4_3 | Betaflight4_4 | Betaflight4_5) => {
+                Some(Self::Brushed)
+            }
             (4u32, Inav5 | Inav6 | Inav7) => Some(Self::Dshot150),
-            (5u32, Betaflight4_2 | Betaflight4_3 | Betaflight4_4) => Some(Self::Dshot150),
+            (5u32, Betaflight4_2 | Betaflight4_3 | Betaflight4_4 | Betaflight4_5) => {
+                Some(Self::Dshot150)
+            }
             (5u32, Inav5 | Inav6 | Inav7) => Some(Self::Dshot300),
-            (6u32, Betaflight4_2 | Betaflight4_3 | Betaflight4_4) => Some(Self::Dshot300),
+            (6u32, Betaflight4_2 | Betaflight4_3 | Betaflight4_4 | Betaflight4_5) => {
+                Some(Self::Dshot300)
+            }
             (6u32, Inav5 | Inav6 | Inav7) => Some(Self::Dshot600),
-            (7u32, Betaflight4_2 | Betaflight4_3 | Betaflight4_4) => Some(Self::Dshot600),
-            (8u32, Betaflight4_2 | Betaflight4_3 | Betaflight4_4) => Some(Self::Proshot1000),
-            (9u32, Betaflight4_2 | Betaflight4_3 | Betaflight4_4) => Some(Self::Disabled),
+            (7u32, Betaflight4_2 | Betaflight4_3 | Betaflight4_4 | Betaflight4_5) => {
+                Some(Self::Dshot600)
+            }
+            (8u32, Betaflight4_2 | Betaflight4_3 | Betaflight4_4 | Betaflight4_5) => {
+                Some(Self::Proshot1000)
+            }
+            (9u32, Betaflight4_2 | Betaflight4_3 | Betaflight4_4 | Betaflight4_5) => {
+                Some(Self::Disabled)
+            }
             _ => {
                 #[allow(clippy::redundant_closure_call)]
                 (|raw| tracing::error!("invalid pwm protocol: {raw}"))(raw);
