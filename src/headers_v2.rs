888f358587eab627771ec08c75e055023a1f241f
Wren Etheredge <me@wetheredge.com>
2025-04-22 17:16:54 -0400

    headers: Add tests

--- src/headers_v2.rs
+++ src/headers_v2.rs
@@ -33,6 +33,7 @@ impl<'data> Iterator for HeadersParser<'data> {
         if self.data.peek() != Some(b'H') {
             return None;
         }
+        let _ = self.data.read_u8();
 
         // cannot be empty after peek() is Some
         let line = self.data.read_line().unwrap();
@@ -46,3 +47,39 @@ impl<'data> Iterator for HeadersParser<'data> {
 }
 
 impl FusedIterator for HeadersParser<'_> {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn header<'n, 'v>(
+        name: &'n [u8],
+        value: &'v [u8],
+    ) -> Result<(&'n [u8], &'v [u8]), MissingColonError> {
+        Ok((name, value))
+    }
+
+    #[test]
+    fn valid() {
+        let mut headers = HeadersParser::new(b"H Test:true\nH :empty header\nH Empty value:\nbody");
+        assert_eq!(header(b"Test", b"true"), headers.next().unwrap());
+        assert_eq!(header(b"", b"empty header"), headers.next().unwrap());
+        assert_eq!(header(b"Empty value", b""), headers.next().unwrap());
+        assert!(headers.next().is_none());
+        assert_eq!(Some(b"body".as_slice()), headers.data.read_line());
+    }
+
+    #[test]
+    fn corrupted() {
+        let mut headers = HeadersParser::new(&[
+            72, 32, 72, 97, 115, 58, 110, 117, 0, 108, 108, 10, 72, 32, 78, 111, 110, 45, 117, 116,
+            102, 56, 58, 0, 159, 146, 150, 10, 98, 111, 100, 121,
+        ]);
+        assert_eq!(header(b"Has", b"nu\0ll"), headers.next().unwrap());
+        assert_eq!(
+            header(b"Non-utf8", &[0, 159, 146, 150]),
+            headers.next().unwrap()
+        );
+        assert!(headers.next().is_none());
+    }
+}
